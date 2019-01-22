33674f98588c971f66a97e433e1f78572d3cfa22
Stefanie Loh <stefkloh@gmail.com>
2019-01-21 22:45:37 -0700

    Created mock heroes, added style, display hero details on click

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,26 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-// Import Hero class
-import { Hero } from '../hero';
+import { Hero } from '../hero'; // Import Hero class
+import { HEROES } from '../mock-heroes'; // Import the mock HEROES - Displays the list of heroes at the top of the HeroesComponent
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent implements OnInit {
 
-  // Add a hero property
-  //hero = 'Windstorm';
+  // In the same file (HeroesComponent class), define a component property called heroes to expose HEROES array for binding
+  heroes = HEROES;
 
-  // Refactor the component's hero property to be of type Hero. Initialize it with an id of 1 and the name Windstorm.
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  // Rename the component's hero property to selectedHero but don't assign it. There is no selected hero when the application starts
+  selectedHero: Hero;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  // Add the following onSelect() method, which assigns the clicked hero from the template to the component's selectedHero
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
 }
+
